AOSPA/android_device_oneplus_oneplus7pro

oneplus7pro: Add support for T model
Change-Id: I06b17d15fe9e049c397650e67a13799061b691bb

BoardConfig.mk
@@ -20,7 +20,9 @@
 DEVICE_PATH := device/oneplus/oneplus7pro
 
 # Recovery
+ifneq ($(TARGET_ONEPLUS_T_DEVICE),true)
 TARGET_PREBUILT_RECOVERY_RAMDISK_CPIO := $(DEVICE_PATH)/ramdisk-recovery.cpio
+endif
 
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml

device.mk
@@ -14,7 +14,11 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+ifneq ($(TARGET_ONEPLUS_T_DEVICE),true)
+PRODUCT_SHIPPING_API_LEVEL := 28
+else
+PRODUCT_SHIPPING_API_LEVEL := 29
+endif
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/oneplus/oneplus7pro/oneplus7pro-vendor.mk)
@@ -33,6 +37,14 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-pa
+
+ifneq ($(TARGET_ONEPLUS_T_DEVICE),true)
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-specific/nonT
+else
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-specific/T
+endif
 
 # Properties
 -include $(LOCAL_PATH)/device-props.mk

AOSPA/android_device_oneplus_oneplus7

BoardConfig.mk
@@ -20,7 +20,9 @@
 DEVICE_PATH := device/oneplus/oneplus7pro
 
 # Recovery
+ifneq ($(TARGET_IS_ONEPLUS_T_DEVICE),true)
 TARGET_PREBUILT_RECOVERY_RAMDISK_CPIO := $(DEVICE_PATH)/ramdisk-recovery.cpio
+endif
 
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml

device.mk
@@ -14,7 +14,11 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+ifneq ($(TARGET_IS_ONEPLUS_T_DEVICE),true)
+PRODUCT_SHIPPING_API_LEVEL := 28
+else
+PRODUCT_SHIPPING_API_LEVEL := 29
+endif
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/oneplus/oneplus7pro/oneplus7pro-vendor.mk)
@@ -33,6 +37,14 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-pa
+
+ifneq ($(TARGET_IS_ONEPLUS_T_DEVICE),true)
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-specific/op7
+else
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-specific/op7t
+endif
 
 # Properties
 -include $(LOCAL_PATH)/device-props.mk