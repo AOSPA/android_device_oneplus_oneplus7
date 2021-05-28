410300d3e7298f7c8e6e56cf966a436ea378f61e
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-05-28 03:26:47 +0000

    oneplus7: switch to RROs
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- device.mk
+++ device.mk
@@ -10,9 +10,11 @@ $(call inherit-product, device/oneplus/sm8150-common/common.mk)
 $(call inherit-product, vendor/oneplus/oneplus7/oneplus7-vendor.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aospa
+PRODUCT_PACKAGES += \
+    AOSPAOnePlus7SystemUI \
+    OnePlus7Frameworks \
+    OnePlus7SettingsProvider \
+    OnePlus7SystemUI
 
 # Audio
 PRODUCT_COPY_FILES += \
