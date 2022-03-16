a95bfebe7dd376704884f0f543137697f992c7ef
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-03-16 18:00:30 +0900

    oneplus7: commonize non dynamic configs
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -21,10 +21,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_pahu.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_pahu.xml \
     $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml
 
-# Device init scripts
-PRODUCT_PACKAGES += \
-    fstab.qcom
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
